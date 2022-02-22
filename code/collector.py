21b6c71be51fa320f4b841c67133674fc02031c1
Roman Pridybailo <rp@vaix.ai>
2022-02-22 18:08:46 +0200

    first

--- code/collector.py
+++ code/collector.py
@@ -6,7 +6,7 @@ import os
 import sys
 
 try:
-  loadBalancerId = os.environ['LOAD_BALANCER_ID']
+  loadBalancerIds = os.environ['LOAD_BALANCER_IDS']
 except KeyError:
   print('Variable LOAD_BALANCER_ID not defined')
   sys.exit(1)
@@ -18,10 +18,10 @@ except KeyError:
   sys.exit(1)
 
 
-requestUrl = 'https://api.hetzner.cloud/v1/load_balancers/' + loadBalancerId
+hetznerCloudApiUrl = 'https://api.hetzner.cloud/v1/load_balancers/'
 
 def getLoadBalancerType(id):
-    url = requestUrl
+    url = hetznerCloudApiUrl + id
 
     headers = {
         'Content-type': "application/json",
@@ -32,12 +32,12 @@ def getLoadBalancerType(id):
     return get.json()
 
 
-def getMetrics(metricsType):
+def getMetrics(metricsType, id):
     utc_offset_sec = time.altzone if time.localtime().tm_isdst else time.timezone
     utc_offset = datetime.timedelta(seconds=-utc_offset_sec)
     hetznerDate = datetime.datetime.now().replace(tzinfo=datetime.timezone(offset=utc_offset)).isoformat()
 
-    url = requestUrl + '/metrics'
+    url = hetznerCloudApiUrl + id + '/metrics'
 
     headers = {
         'Content-type': "application/json",
@@ -57,20 +57,39 @@ def getMetrics(metricsType):
 
 if __name__ == '__main__':
 
-  try:
-    loadBalancerName = getLoadBalancerType(loadBalancerId)['load_balancer']['name']
-  except Exception as e:
-    print('Couldnt get field', e )
-    sys.exit(1)
+  print('Hetzner Load Balancer Exporter is starting ...')
 
-  for services in getLoadBalancerType(loadBalancerId)['load_balancer']['services']:
-      if services['protocol'] == 'http':
-          lbType = 'http'
-      else:
-          lbType = 'tcp'
+  loadBalanaceridsList = list(loadBalancerIds.split(","))
+
+  print('Getting Info from Hetzner ...\n')
+
+  if len(loadBalanaceridsList) <= 1:
+    print('Found Load Balancer:')
+  else:
+    print('Found Load Balancers:')
+
+  loadBalanacerFullList = []
+
+  for loadBalancerId in loadBalanaceridsList:
+    try:
+      loadBalancerName = getLoadBalancerType(loadBalancerId)['load_balancer']['name']
+    except Exception as e:
+      print('Couldnt get field', e )
+      sys.exit(1)
 
 
-  HetznerLoadBalancerInfo = Info('hetzner_load_balancer', 'Hetzner Load Balancer Exporter build info', ['hetzner_load_balancer_id', 'hetzner_load_balancer_name'])
+    for services in getLoadBalancerType(loadBalancerId)['load_balancer']['services']:
+        if services['protocol'] == 'http':
+            lbType = 'http'
+        else:
+            lbType = 'tcp'
+    
+    loadBalanacerFullList.append([loadBalancerId, loadBalancerName, lbType])
+
+    print('\n\tName:\t' + loadBalancerName + '\n\tId:\t' + loadBalancerId + '\n\tType:\t' + lbType)
+
+
+  HetznerLoadBalancerInfo = Info('hetzner_load_balancer', 'Hetzner Load Balancer Exporter build info')
   HetznerOpenConnections = Gauge('hetzner_load_balancer_open_connections', 'Open Connections on Hetzner Load Balancer', ['hetzner_load_balancer_id', 'hetzner_load_balancer_name'])
   HetznerConnectionsPerSecond = Gauge('hetzner_load_balancer_connections_per_second', 'Connections per Second on Hetzner Load Balancer', ['hetzner_load_balancer_id', 'hetzner_load_balancer_name'])
   HetznerRequestsPerSecond = Gauge('hetzner_load_balancer_requests_per_second', 'Requests per Second on Hetzner Load Balancer', ['hetzner_load_balancer_id', 'hetzner_load_balancer_name'])
@@ -78,18 +97,21 @@ if __name__ == '__main__':
   HetznerBandwidthOut = Gauge('hetzner_load_balancer_bandwidth_out', 'Bandwidth out on Hetzner Load Balancer', ['hetzner_load_balancer_id', 'hetzner_load_balancer_name'])
 
   start_http_server(8000)
-  print('Web server started on port 8000')
+  print('Hetzner Load Balancer Exporter started on port 8000\n')
 
   while True:
-    HetznerLoadBalancerInfo.labels(hetzner_load_balancer_id=loadBalancerId, hetzner_load_balancer_name=loadBalancerName).info({'version': '0.0.1', 'buildhost': 'drake0103@gmail.com'})
-    try:
-      HetznerOpenConnections.labels(hetzner_load_balancer_id=loadBalancerId, hetzner_load_balancer_name=loadBalancerName).set_function(lambda: getMetrics('open_connections')["metrics"]["time_series"]["open_connections"]["values"][0][1])
-    except:
-      print('Couldnt get field', e )
-    HetznerConnectionsPerSecond.labels(hetzner_load_balancer_id=loadBalancerId, hetzner_load_balancer_name=loadBalancerName).set_function(lambda: getMetrics('connections_per_second')["metrics"]["time_series"]["connections_per_second"]["values"][0][1])
-    if lbType == 'http':
-      HetznerConnectionsPerSecond.labels(hetzner_load_balancer_id=loadBalancerId, hetzner_load_balancer_name=loadBalancerName).set_function(lambda: getMetrics('requests_per_second')["metrics"]["time_series"]["requests_per_second"]["values"][0][1])
-    HetznerBandwidthIn.labels(hetzner_load_balancer_id=loadBalancerId, hetzner_load_balancer_name=loadBalancerName).set_function(lambda: getMetrics('bandwidth')["metrics"]["time_series"]["bandwidth.in"]["values"][0][1])
-    HetznerBandwidthOut.labels(hetzner_load_balancer_id=loadBalancerId, hetzner_load_balancer_name=loadBalancerName).set_function(lambda: getMetrics('bandwidth')["metrics"]["time_series"]["bandwidth.out"]["values"][0][1])
+    HetznerLoadBalancerInfo.info({'version': '2.0.0', 'buildhost': 'drake0103@gmail.com'})
+    for loadBalanacerFullInfo in loadBalanacerFullList:
+      try:
+        # HetznerOpenConnections.labels(hetzner_load_balancer_id=loadBalanacerFullInfo[0], hetzner_load_balancer_name=loadBalanacerFullInfo[1]).set_function(lambda: getMetrics('open_connections',loadBalanacerFullInfo[0])["metrics"]["time_series"]["open_connections"]["values"][0][1])
+        HetznerOpenConnections.labels(hetzner_load_balancer_id=loadBalanacerFullInfo[0],hetzner_load_balancer_name=loadBalanacerFullInfo[1]).set(getMetrics('open_connections',loadBalanacerFullInfo[0])["metrics"]["time_series"]["open_connections"]["values"][0][1])
+      except:
+        print('Couldnt get field', e )
+      HetznerConnectionsPerSecond.labels(hetzner_load_balancer_id=loadBalanacerFullInfo[0], hetzner_load_balancer_name=loadBalanacerFullInfo[1]).set_function(lambda: getMetrics('connections_per_second',loadBalanacerFullInfo[0])["metrics"]["time_series"]["connections_per_second"]["values"][0][1])
+      #HetznerConnectionsPerSecond.labels(hetzner_load_balancer_id=loadBalanacerFullInfo[0], hetzner_load_balancer_name=loadBalanacerFullInfo[1]).set(getMetrics('connections_per_second',loadBalanacerFullInfo[0])["metrics"]["time_series"]["connections_per_second"]["values"][0][1])
+      if loadBalanacerFullInfo[2] == 'http':
+        HetznerConnectionsPerSecond.labels(hetzner_load_balancer_id=loadBalanacerFullInfo[0], hetzner_load_balancer_name=loadBalanacerFullInfo[1]).set(getMetrics('requests_per_second',loadBalanacerFullInfo[0])["metrics"]["time_series"]["requests_per_second"]["values"][0][1])
+      HetznerBandwidthIn.labels(hetzner_load_balancer_id=loadBalanacerFullInfo[0], hetzner_load_balancer_name=loadBalanacerFullInfo[1]).set(getMetrics('bandwidth',loadBalanacerFullInfo[0])["metrics"]["time_series"]["bandwidth.in"]["values"][0][1])
+      HetznerBandwidthOut.labels(hetzner_load_balancer_id=loadBalanacerFullInfo[0], hetzner_load_balancer_name=loadBalanacerFullInfo[1]).set(getMetrics('bandwidth',loadBalanacerFullInfo[0])["metrics"]["time_series"]["bandwidth.out"]["values"][0][1])
 
-    time.sleep(1)
+    time.sleep(15)
